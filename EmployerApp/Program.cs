33e37fa6c2380a04b690e2a9225fdc77e3ce745b
VPHQ\aerkmen <abdulkadir.erkmen@veripark.com>
2018-07-26 13:18:30 +0300

    Add project files.

---
+++ EmployerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
